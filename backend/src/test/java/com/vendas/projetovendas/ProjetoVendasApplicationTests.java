66f1ef5cb36ee5c8112452e26dbdefeac57e847a
Rodrigo Paim <rodrigosoarespaim@gmail.com>
2022-09-09 19:49:17 -0300

    Project Created

---
+++ backend/src/test/java/com/vendas/projetovendas/ProjetoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendas.projetovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
