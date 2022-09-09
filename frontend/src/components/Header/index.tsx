337d1257890aa8ba45565d4b1f1e3fcd33575f67
Rodrigo Paim <rodrigosoarespaim@gmail.com>
2022-09-09 20:53:49 -0300

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="LogoDSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/digospaim">@digospaim</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
