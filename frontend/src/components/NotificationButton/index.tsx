106369629cfb5a51081db4a216d1ab58283d7caa
Rodrigo Paim <rodrigosoarespaim@gmail.com>
2022-09-10 01:23:09 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notifcation-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -13,7 +14,7 @@ function handleClick(id : number){
 
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            console.log("ENVIADO");
+            toast.info("SMS Enviado com Sucesso!");
         });
 
 }
